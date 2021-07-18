d09f24034cde935cb494e8c0b68a9a63b78cc116
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-18 08:26:38 -0700

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="(table, index) in tables" :key="table.id">
+      <p>{{index}}. {{table.name}}</p>
+      <p>{{index}}. {{table.price }}</p>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      message2: "Welcome again",
+      tables: [
+        {id: 1, name: "Product 1", price: 10},
+        {id: 2, name: "Product 2", price: 230},
+      ],
+    };
+  },
+  created: function() {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:8080").then(response => { console.log(response.data);
+        this.products = response.data;
+      })
+    }
   },
 };
 </script>
