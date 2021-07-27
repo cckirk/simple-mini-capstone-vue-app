aeafb09c9680808a4d2850f8eb40148e4b6d47e5
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-26 18:56:23 -0700

    adding css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,92 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
+  <body>  
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link>
+        |<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
     </div>
-    <router-view />
-  </div>
+  </nav>
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
+    </div>
+  </body>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+h1 {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  font-size: 24px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 700;
+  line-height: 26.4px;
 }
-
-#nav {
-  padding: 30px;
+h3 {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 700;
+  line-height: 15.4px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+p {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 20px;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+blockquote {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  font-size: 21px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 30px;
+}
+pre {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  font-size: 13px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 18.5714px;
+}
+body {
+  background-image: url(moroccan-flower-dark.png);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,68 @@
 <template>
-
   <div class="home">
-    
-    <header>Access-Control-Allow-Origin: *</header>
-    <h1>{{ message }}</h1>
-    <p>Product: <input type="text" v-model="newProductParams.name"> </p>
-    <p>Description: <input type="text" v-model="newProductParams.description" ></p>
-    <p>Price: <input type="text" v-model="newProductParams.price"> </p>
-    <p>image_url: <input type="text" v-model="newProductParams.image_url"> </p>
-    <button v-on:click="createProduct()">Create Product</button>
-    <div v-for="product in products" v-bind:key="product.id">
+    <h1>Simple Products App</h1>
+    <h2>Create a new Product</h2>
+    <p>Name: <input type="text" v-model="newProduct.name" /></p>
+    <p>Description: <input type="text" v-model="newProduct.description" /></p>
+    <p>Price: <input type="text" v-model="newProduct.price" /></p>
+    <p>image_url: <input type="text" v-model="newProduct.image_url" /></p>
+    <button v-on:click="createProduct">Add Product</button>
+    <hr />
+    <div v-for="product in sortedProducts" v-bind:key="product.id">
       <p>Product: {{ product.id }}</p>
-      <p>name: {{ product.name }}</p>
-      <p>description: {{ product.description }}</p>
+      <p>{{ product.name }}</p>
+      <img v-bind:src="product.image_url" />
+      <p>Description: {{ product.description }}</p>
       <p>Price: {{ product.price }}</p>
-      <p>Image Url: {{ product.image_url }}</p>
-      <img v-bind:src="product.image_url">
-      <p><button v-on:click="productsCreate()">Show more info</button></p>
+      <p><button v-on:click="showProduct(product)">Show Info</button></p>
+      <p><button v-on:click="editProduct(product)">Update</button></p>
+      <hr />
     </div>
-    <p><button v-on:click="showProduct(product)">Show more info</button></p>
     <dialog id="product-details">
       <form method="dialog">
-        <p>Product: <input type="text" v-model="newProductParams.id"></p>
-        <p>Price: <input type="text" v-model="newProductParams.price"></p>
-        <p>Description: <input type="text" v-model="newProductParams.description"></p>
-        <p>img_url: <input type="text" v-model="newProductParams.img_url"></p>
-        <button>close</button>
-        <button v-on:click="updateProduct(currentProduct)"> Update Product </button>
+        <h2>{{ currentProduct.name }}</h2>
+        <img v-bind:src="currentProduct.image_url" />
+        <p>{{ currentProduct.description }}</p>
+        <p>Price: {{ currentProduct.price }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
+    <dialog id="product-update">
+      <form method="dialog">
+        <h2>{{ currentProduct.name }}</h2>
+        <p>Name: <input type="text" v-model="currentProduct.name" /></p>
+        <p>
+          Description:
+          <input type="text" v-model="currentProduct.description" />
+        </p>
+        <p>Price: <input type="text" v-model="currentProduct.price" /></p>
+        <p>
+          Image URL: <input type="text" v-model="currentProduct.image_url" />
+        </p>
+        <p>
+          <button v-on:click="updateProduct(currentProduct)">Update</button>
+        </p>
+        <p>
+          <button v-on:click="destroyProduct(currentProduct)">Delete</button>
+        </p>
       </form>
     </dialog>
   </div>
 </template>
 <style>
 img {
-  width: 250px;
+  height: 250px;
 }
 </style>
-<style></style>
 <script>
 import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      message2: "Welcome again",
       products: [],
-      newProductParams: {},
+      newProduct: {},
       currentProduct: {},
-      theProduct: {},
+      errors: {},
     };
   },
   created: function () {
@@ -60,32 +75,36 @@ export default {
       });
     },
     createProduct: function () {
-      var productParams = {
-        description: this.newProductParams.description,
-        price: this.newProductParams.price,
-        image_url: this.newProductParams.image_url,
-        name: this.newProductParams.name,
-      };
-      axios.post("http://localhost3000/products", productParams).then((response) => {
-        this.products.push(response.data);
-        this.newProductParams = {};
-      });
+      axios
+        .post("http://localhost:3000/products", this.newProduct)
+        .then((response) => {
+          this.products.push(response.data);
+          this.newProduct = {};
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
     },
-    showProduct: function (theProduct) {
-      this.currentProduct = theProduct;
-      console.log("showing product");
+    showProduct: function (product) {
+      this.currentProduct = product;
+      console.log(this.currentProduct);
       document.querySelector("#product-details").showModal();
     },
-    updateProduct: function (theProduct) {
-      var editParams = theProduct;
-      axios.patch("http://localhost:3000/products/" + editParams.id, editParams).then((response) => {
+    editProduct: function (product) {
+      this.currentProduct = product;
+      document.querySelector("#product-update").showModal();
+    },
+    updateProduct: function (product) {
+      axios.patch(`http://localhost:3000/products/${product.id}`, this.currentProduct).then((response) => {
         console.log(response.data);
       });
     },
-    destroyProduct: function (theProduct) {
-      axios.delete("http://localhost:3000/products/${theProduct.id}").then((response) => {
-        var index = this.products.indexOf(theProduct);
+    destroyProduct: function (product) {
+      axios.delete(`http://localhost:3000/products/${product.id}`).then((response) => {
+        var index = this.products.indexOf(product);
         this.products.splice(index, 1);
+        console.log(response.data);
       });
     },
   },
